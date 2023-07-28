0c7f07553d7a4b2844c8e81f62f78357034f0902
Protullen <138602811+protullen@users.noreply.github.com>
2023-07-28 15:16:14 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,6 +1,6 @@
 class Config(object):
-    APP_ID = 000
-    API_HASH = "000"
+    APP_ID = 11427887
+    API_HASH = "317ea7bfc3dcbfb2a4aa7c866110074e"
     BOT_TOKEN = "000"
     BOT_WORKERS = 40
     SUDO_USERS = "000,111,222,333"
